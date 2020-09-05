6bc66231556ea21f8a5ab57fecfcfef5af42b96a
antogit-sys <antonioperrgiorgi@gmail.com>
2020-09-05 16:36:20 +0200

    fixed bud & pulizia stdin

--- macrolib.h
+++ macrolib.h
@@ -1,7 +1,17 @@
+#include <stdbool.h>
+
 #if __linux__	//se è un sistema linux dichiara
+#	include <stdio_ext.h>	
 #	define PULISCI "clear"
+#	define stdinclear() __fpurge(stdin)
 #elif __APPLE__ //altrimenti se è un sistema apple dichiara
+#	include <stdio_ext.h>
 #	define PULISCI "clear"
+#	define stdinclear() __fpurge(stdin)
 #elif _WIN32 || WIN64 || CYGWIN || CYGWIN32__ //altrimenti se è un sistema winzzoz dichiara
 #	define PULISCI "cls"
+#	define stdinclear() fflush(stdin)
 #endif
+
+
+
--- trismain.c
+++ trismain.c
@@ -1,21 +1,30 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
 
 #include "trislib.h"
+#include "macrolib.h"
 
 int main()
 {
 	int r=0,c=0,i=0;
+	bool errore;
+	int ctrl1,ctrl2;
 	char opz;
-	char grid[5][6]={
-  // 1 2 3
-	" | | ", 
+	
+	char grid[6][6]={
+//	 1 2 3
+	" | | ", // 1 
 	"-----", 
-	" | | ", 
+	" | | ", // 2
 	"-----", 
-	" | | ", 
+	" | | ", // 3
 	};
-	int z=0;
+	
+	//int lol;
+	//int merda=scanf("%d",&lol);
+	//printf("merda: %d",merda);
+	//return 0;
 	while(i<9){
 		
 		turno(&opz,i); //ogni ciclo cambia turno
@@ -24,11 +33,17 @@ int main()
 			grafic(grid);
 			printf("[num da 1 a 3]\n");
 			printf("inserisci riga: ");
-			scanf("%d",&r);
+			ctrl1=scanf("%d",&r); 
 			printf("inserisci colonna: ");
-			scanf("%d",&c);
-
-		}while((r>=1 || r<=3) && (c>=1 || c<=3) && controllGrid(grid,r,c)==-1);
+			ctrl2=scanf("%d",&c);
+			
+			if(ctrl1==0 && ctrl2==0){
+				errore=true;
+			}else{
+				errore=false;
+			}
+			stdinclear(); //pulisco canale stdin
+		}while(((r>=1 || r<=3) && (c>=1 || c<=3) && controllGrid(grid,r,c)==-1) || errore==true || (r==4 || c==4)); //(((r<1 || r>3) && (c<1 || c>3)) && errore!=FALSE && controllGrid(grid,r,c)==-1);
 		
 		aggiustacordinate(&r,&c);
 		grid[r][c]=opz;
